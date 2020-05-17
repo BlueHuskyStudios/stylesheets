3e75b5ea6ff488b28b50116946de0a8eafcd364c
Ben Leggiero <BenLeggiero@Gmail.com>
2020-05-17 01:16:46 -0600

    Many improvements I made for Recipes.BenLeggiero.me

--- runServer.sh
+++ runServer.sh
@@ -8,4 +8,4 @@ if [ 0 -ne $bundleCheckStatus ]; then
 fi
 
 # jekyll serve --trace --incremental
-bundle exec jekyll serve --trace --incremental
+bundle exec jekyll serve --port 4001 --trace --incremental
